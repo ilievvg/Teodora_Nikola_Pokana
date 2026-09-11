98ff70bac8c8d1cc95910e5afadd45385b629767
ilievvg <gligoriliev0@gmail.com>
2026-09-11 12:16:23 +0200

    update1

--- js/main.js
+++ js/main.js
@@ -503,21 +503,21 @@ window.addEventListener(
       const atTop = window.scrollY <= 8;
       const atBottom = window.innerHeight + window.scrollY >= document.documentElement.scrollHeight - 8;
 
-      if (deltaY > 0 && !atTop) {
+      if (deltaY < 0 && !atTop) {
         return;
       }
 
-      if (deltaY < 0 && !atBottom) {
+      if (deltaY > 0 && !atBottom) {
         return;
       }
     }
 
-    if (deltaY > 0) {
-      navigateTo(nextPage, 'next');
+    if (deltaY < 0) {
+      navigateTo(previousPage, 'previous');
       return;
     }
 
-    navigateTo(previousPage, 'previous');
+    navigateTo(nextPage, 'next');
   },
   { passive: true }
 );
